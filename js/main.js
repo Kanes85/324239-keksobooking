948bd39cb32747c31bd5c8e6b27850e7ab2a09c4
Anton <kanes@inbox.ru>
2018-12-03 03:16:51 +0300

    Тестирование перемешивания

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 var USER_AVATAR = 8;
-
 var offerTitle = [
   'Большая уютная квартира',
   'Маленькая неуютная квартира',
@@ -10,33 +9,29 @@ var offerTitle = [
   'Уютное бунгало далеко от моря',
   'Неуютное бунгало по колено в воде'
  ];
-
  var typeMas = ['palace', 'flat', 'house', 'bungalo'];
-
  var checkMas = ['12:00', '13:00', '14:00'];
-
  var featuresMas = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-
  var photosMas = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+ // var photosMas = [1, 2, 3];
 
-
-
-// var getRandomElement = function () {
-// featuresMas.sort( function() { return 0.5 - Math.random() } );
-// };
-//
-// getRandomElement();
+// Получение диапазона price
+var getPriceHouse = function (min, max, interval) {
+  var randomePriceElement = Math.floor(Math.random()*(max-min+interval)/interval);
+  var randomePrice = randomePriceElement * interval + min;
+  return randomePrice;
+};
 
  var getRandomNumber = function (property) {
    var numberGenerator = property[Math.floor(Math.random() * property.length)];
    return numberGenerator;
  };
 
+// Генерация случайного числа в заданном диапазоне
  var getRandomNumb = function (min, max) {
-   var rand = Math.ceil(Math.random() * (min - max) + max)
+   var rand = Math.ceil(Math.random() * (min - max) + max);
    return rand;
    };
-
 
  var getAvatarImage = function () {
     var avatarImages = [];
@@ -51,55 +46,81 @@ var getRandomNumb = function (min, max) {
   return rand;
   };
 
+// Количество комнат в доме
+var houesRooms = getRandomNumb(0, 5);
 
-  var virt = getAvatarImage();
-  var avat = 'img/avatars/' + getRandomNumber(virt);
-  // var offer = getRandomNumber(offerTitle);
-  // var price = getRandomNumb(1000, 100000); //круглые значения с шагом 1000
-  // var type = getRandomNumber(typeMas); //нужно переводить на русский?
-  // var rooms = getRandomNumb(0, 5);
-  // var guests = getRandomNumb(1, 15); //Гостей иногда слишком много!
-  // var checkin = getRandomNumber(checkMas);
-  // var checkout = getRandomNumber(checkMas);
-  // var features = getRandomNumber(featuresMas); //вывести несколько массивов!
-  // var photos = getRandomNumber(photosMas); //Должны выводиться все три в произвольном порядке
-  // var locationX =
-  // var locationY = getRandomNumb(130, 630);
-  // console.log(avat);
-  // console.log(offer);
-  // console.log(price);
-  // console.log(type);
-  // console.log(rooms);
-  // console.log(guests);
-  // console.log(checkin);
-  // console.log(checkout);
-  // console.log(features);
-  // console.log(photos);
-  // console.log(locationX);
-  // console.log(locationY);
+// Определяем количество гостей в доме
+var getGuestsInHouse = function () {
+ var guestsInRoom = Math.ceil(Math.random() * 4);
+ var roomsInHouse = houesRooms;
+ var guestInHouse = guestsInRoom * roomsInHouse;
+ return guestInHouse;
+};
+
+// Перемешивание массива photos
+// var photosMas = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+// function compareRandom() {
+//   return Math.random() - 0.5;
+// }
+
+function random (min, max, l) {
+  var arr = [], m = [], n = 0;
+  if (max - min < l-1) return;
+  for(var i=0; i<=(max-min); i++)
+  m[i] = i + min;
+  for (var i=0; i<l; i++) {
+    n = Math.floor(Math.random()*(m.length));
+    arr[i]= m.splice(n, 1)[0];};
+  return arr
+}
+// console.log(JSON.stringify(random(0,2,3)));
+
+var photo = function () {
+  var mas = [];
+  // for(var i = 0; i < photosMas.length; i++) {
+    mas.push(random(0,2,3));
+  // }
+  // mas = i;
+photosMas
+  console.log(mas);
+  // return mas;
+};
+photo();
 
 
+// function random (min, max, l) {
+//   var arr = [], m = [], n = 0;
+//   if (max - min < l-1) return;
+//   for(var i=0; i<=(max-min); i++)
+//   m[i] = i + min;
+//   for (var i=0; i<l; i++) {
+//     n = Math.floor(Math.random()*(m.length));
+//     arr[i]= m.splice(n,1)[0];};
+//   return arr
+// }
+// console.log(JSON.stringify(random(0,2,3)));
 
 var massiv = [
   {
     author: {
-      avatar: avat
+      avatar: 'img/avatars/' + getRandomNumber(getAvatarImage())
     },
     offer: {
-      title: getRandomNumber(offerTitle),
-      address: '600, 350',
-      price: getRandomNumb(1000, 100000),
+      title: getRandomNumber(offerTitle),  //Значения не должны повторяться
+      address: '600, 350',  //"{{location.x}}, {{location.y}}", например, "600, 350"
+      price: getPriceHouse(1000, 1000000, 1000),
       type: getRandomNumber(typeMas),
-      rooms: getRandomNumb(0, 5),
-      guests: getRandomNumb(1, 15),
+      rooms: houesRooms,
+      guests: getGuestsInHouse(),
       checkin: getRandomNumber(checkMas),
       checkout: getRandomNumber(checkMas),
-      features: getRandomNumber(featuresMas),
+      features: getRandomNumber(featuresMas), //ассив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"
       description: ' ',
-      photos: getRandomNumber(photosMas),
+      // photos: photosMas.sort(compareRandom),
+      // photos: random(0,2,3)
     },
     location: {
-      x: 360,
+      x: 360, //«x»: случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
       y: getRandomNumb(130, 630)
     }
   }
